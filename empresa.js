3c661bcc703bf7147ca4c3b716a7ab0dfb64df82
improvola <83605546+improvola@users.noreply.github.com>
2023-01-28 21:28:40 -0300

    Add files via upload

---
+++ empresa.js
@@ -0,0 +1,66 @@
+let empresa = [
+   {
+        id: 1,
+        like:false,
+        nombre: "MTI Bordados Niquitex",
+        anio: "Septiembre 2022 - Actualidad",
+        descripcion: "Diseñadora gráfica e ilustradora para estampado de prendas.",
+        fotoUrl: "img/niquitex.png"
+    },
+    {
+        id: 2,
+        like:false,
+        nombre: "Uniformar-C | Empleada",
+        anio: "Septiembre 2021 - Agosto 2022",
+        descripcion: "Encargada deposito. Preparacion de pedidos y carga de datos.",
+        fotoUrl: "img/uniformarc.png"
+    },
+    {
+        id: 3,
+        like:false,
+        nombre: "Diseñadora Freelance",
+        anio: "Junio 2020 - Actualidad",
+        descripcion: "Diseño de logos, vectores, ilustraciones y escenas por encargo.",
+        fotoUrl: "img/impdesign.png"
+    },
+    {
+        id: 4,
+        like:true,
+        nombre: "AB Automotores | Gestora Automotor",
+        anio: "Enero 2018 - Marzo 2020",
+        descripcion: "Tareas administrativas. Carga de datos y maquetadora. Armado de planillas de vehículos en consignacion.",
+        fotoUrl: "img/ab.png"
+    },
+    {
+        id: 5,
+        like:false,
+        nombre: "GOTITA | Encargada Caja",
+        anio: "Mayo 2017 - Marzo 2020",
+        descripcion: "Manejo de dinero y POSNET, control de stock, apertura y cierre del local. Manejo de redes sociales.",
+        fotoUrl: "img/gotita.png"
+    },
+    {
+        id: 6,
+        like:false,
+        nombre: "GRUM | Organizadora - CM",
+        anio: "Febrero 2017 - Agosto 2019",
+        descripcion: "Logística y organizacion general. Encargada en Diseño Gráfico y CM de redes sociales. Elaboración de campañas publicitarias, Facebook Ads, Instagram Ads.",
+        fotoUrl: "img/grum.png"
+    },
+    {
+        id: 7,
+        like:false,
+        nombre: "Kravi/Brook | Camarera Vip",
+        anio: "Diciembre 2018 - Julio 2019",
+        descripcion: "Camarera en sectores VIPs de discotecas de Palermo y Caballito. Manejo de dinero, atención al público y venta de promociones en carta. Servicio de eventos privados, catering y caja.",
+        fotoUrl: "img/brook.png"
+    },
+    {
+        id: 8,
+        like:true,
+        nombre: "Fast Food Sudamericana | Entrenadora",
+        anio: "Enero 2016 - Marzo 2017",
+        descripcion: "(Obelisco) Atención al cliente y cuidado de factores críticos. Capacitación a empleados. TOP 5 como “Mejor empleado” y “Best Team”.",
+        fotoUrl: "img/bk.png"
+    },
+]
